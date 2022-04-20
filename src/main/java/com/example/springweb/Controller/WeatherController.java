64446b1332bebcdee41f5b1c9f7018c2f382c827
Solveen Gurung <solveengurung@gmail.com>
2022-04-20 13:00:45 +0545

    Weather Description

--- src/main/java/com/example/springweb/Controller/WeatherController.java
+++ src/main/java/com/example/springweb/Controller/WeatherController.java
@@ -1,23 +1,25 @@
 package com.example.springweb.Controller;
 
 
-import com.example.springweb.Model.Weather;
 import com.example.springweb.Service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
-
 @Controller
 public class WeatherController {
     @Autowired
     WeatherService weatherService;
 
+    public WeatherController(WeatherService weatherService) {
+        this.weatherService = weatherService;
+    }
+
     @GetMapping("/")
     public String index(Model model) {
-//    List<Weather> weathers = weatherService.convert();
+        model.addAttribute("weather", weatherService.getCurrentWeather("Sydney","Australia"));
+        model.addAttribute("weathers", weatherService.getCurrentWeathers("Kathmandu","Nepal"));
         return "index";
     }
 
--- src/main/java/com/example/springweb/Service/WeatherService.java
+++ src/main/java/com/example/springweb/Service/WeatherService.java
@@ -5,32 +5,53 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriTemplate;
 
 import java.math.BigDecimal;
+import java.net.URI;
 
 @Service
 public class WeatherService {
 
     //url
-    private final static String WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?q=Kathmandu,Nepal&appid=05950c29b857993bf578618537855ecd";
+    private final static String WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?q={city},{country}&appid={api}";
     //apikey
+    //yesko value application.properties bata aauxa
+    @Value("${api}")
     private String apiKey;
     //restTemplate and objectMapper
     private final RestTemplate restTemplate;
     private final ObjectMapper objectMapper;
+
+
+
     //Constructor
-    //URL ResponseENTITY
-
-
     public WeatherService(RestTemplateBuilder restTemplate, ObjectMapper objectMapper) {
         this.restTemplate = restTemplate.build();
         this.objectMapper = objectMapper;
     }
 
+
+    public Weather getCurrentWeather(String city, String country) {
+        URI uri = null;
+        uri = new UriTemplate(WEATHER_URL).expand(city, country, apiKey);
+        //Ako jati sabai response lai string ma lera janxa
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(uri, String.class);
+        return convert(responseEntity);
+    }
+    //URL ResponseENTITY
+    public Weather getCurrentWeathers(String city, String country) {
+        URI uri = null;
+        uri = new UriTemplate(WEATHER_URL).expand(city, country, apiKey);
+        //Ako jati sabai response lai string ma lera janxa
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(uri, String.class);
+        return convert(responseEntity);
+    }
 
     //JsonNode
     //Yo method le api bata ako value linxa
@@ -38,7 +59,7 @@ public class WeatherService {
         try {
             JsonNode jsonNode = objectMapper.readTree(responseEntity.getBody());
             return new Weather((jsonNode.path("weather").get(0).path("description")).asText(),
-                    BigDecimal.valueOf(jsonNode.path("main").asDouble()),
+                    BigDecimal.valueOf(jsonNode.path("main").path("temp").asDouble()),
                     BigDecimal.valueOf(jsonNode.path("main").path("feels_like").asDouble()));
 
         } catch (JsonProcessingException e) {
@@ -48,4 +69,5 @@ public class WeatherService {
         }
 
     }
+
 }
